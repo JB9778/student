c09e2d2002ad16002837ba861ab493c71b6ca316
JB9778 <najungbae56@naver.com>
2023-09-14 18:03:57 +0900

    Student 생성자 게터세터 추가

--- Student.java
+++ Student.java
@@ -7,6 +7,40 @@ public class Student {
 	private String id;
 	private double grade;
 	
+	public Student(String name, String department, String id, double grade) {
+		super();
+		this.name = name;
+		this.department = department;
+		this.id = id;
+		this.grade = grade;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
 	//생성자 추가
 	
 
