6020b4006f62d23e855a68acce16904f44ac2021
daehanLee <daehanl8635@gmail.com>
2023-09-14 19:52:13 +0900

    StudentManager 수정

--- StudentManager.java
+++ StudentManager.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class StudentManager {
 	private Scanner scanner = new Scanner(System.in);
 	private ArrayList<Student> dept = new ArrayList<Student>();
-	
+	Student std = new Student();
 	private void read() {
 		System.out.println("학생 이름,학과,학번,학점평균 입력하세요.");
 		for (int i=0; i<4; i++) {
@@ -11,17 +11,33 @@ public class StudentManager {
 			String text = scanner.nextLine();
 			StringTokenizer st = new StringTokenizer(text, ",");
 			
+			std.setName(st.nextToken());
+			std.setDepartment(st.nextToken());
+			std.setId(st.nextToken());
+			std.setGrade(Double.parseDouble(st.nextToken()));
+			
+			System.out.println(std);
 			
 			// ArrayList에 저장
-		}			
+			dept.add(std);
+			
+//			System.out.println(std.getName());
+//			System.out.println(std.getDepartment());
+//			System.out.println(std.getId());
+//			System.out.println(std.getGrade());
+		}
 	}
 	
 	private void printAll() { // 일부러 Iterator로 작성해 보았음
+		
+		System.out.println(dept.get(0));
 		Iterator<Student> it = dept.iterator();
 		while (it.hasNext()) {
-			
 			//학생 정보 출력
-		}		
+			Student stdInfo = it.next();
+			System.out.println(stdInfo);
+			System.out.println("==================");
+		}
 	}
 
 	private void processQuery() {
